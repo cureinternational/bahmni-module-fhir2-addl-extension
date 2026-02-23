641a3d14331c6ab78b68e8fdf84b45c23e052210
SasikiranJ <sasikiran.jakkula@thoughtworks.com>
2026-02-23 17:04:07 +0530

    Add support to update the order status and track the status change using task

---
+++ api/src/main/java/org/bahmni/module/fhir2AddlExtension/api/service/BahmniFhirTaskService.java
@@ -0,0 +1,7 @@
+package org.bahmni.module.fhir2AddlExtension.api.service;
+
+import org.openmrs.module.fhir2.api.FhirTaskService;
+
+public interface BahmniFhirTaskService extends FhirTaskService {
+	
+}
---
+++ api/src/test/java/org/bahmni/module/fhir2AddlExtension/api/utils/TaskStatusToFulfillerStatusMapperTest.java
@@ -0,0 +1,46 @@
+package org.bahmni.module.fhir2AddlExtension.api.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+import org.junit.Test;
+import org.openmrs.Order;
+import org.openmrs.module.fhir2.model.FhirTask;
+
+public class TaskStatusToFulfillerStatusMapperTest {
+	
+	@Test
+	public void shouldMapRequestedToReceived() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(FhirTask.TaskStatus.REQUESTED),
+		    equalTo(Order.FulfillerStatus.RECEIVED));
+	}
+	
+	@Test
+	public void shouldMapAcceptedToInProgress() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(FhirTask.TaskStatus.ACCEPTED),
+		    equalTo(Order.FulfillerStatus.IN_PROGRESS));
+	}
+	
+	@Test
+	public void shouldMapCompletedToCompleted() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(FhirTask.TaskStatus.COMPLETED),
+		    equalTo(Order.FulfillerStatus.COMPLETED));
+	}
+	
+	@Test
+	public void shouldMapRejectedToException() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(FhirTask.TaskStatus.REJECTED),
+		    equalTo(Order.FulfillerStatus.EXCEPTION));
+	}
+	
+	@Test
+	public void shouldReturnNullForUnknownStatus() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(FhirTask.TaskStatus.UNKNOWN), nullValue());
+	}
+	
+	@Test
+	public void shouldReturnNullForNullStatus() {
+		assertThat(TaskStatusToFulfillerStatusMapper.toFulfillerStatus(null), nullValue());
+	}
+}
